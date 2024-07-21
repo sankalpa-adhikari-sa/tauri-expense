e124df2680e00d43a93270c23bc4095db0ccba29
sankalpa Adhikari <sankalpa2522@gmail.com>
2024-07-21 14:27:09 +0545

    initial setup

---
+++ src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+export const Route = createRootRoute({
+  component: () => (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Outlet />
+      <TanStackRouterDevtools />
+    </ThemeProvider>
+  ),
+});
